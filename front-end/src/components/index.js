5908e8cea8e253e46b463f04a7985919b6fabdfe
전솔 <jeonsol12@naver.com>
2019-12-07 21:44:13 +0900

    import 순서대로 정리

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -1,27 +1,27 @@
-import Search from './Search';
-import Country from './Country';
 import Continent from './Continent';
-import Layer from './Layer';
-import Tab from './Tab';
-import Profile from './Profile';
-import Select from './Select';
+import Country from './Country';
 import Currency from './Currency';
 import Expense from './Expense';
-import Report from './Report';
 import Header from './Header';
+import Layer from './Layer';
+import Profile from './Profile';
+import Report from './Report';
+import Search from './Search';
+import Select from './Select';
+import Tab from './Tab';
 import TripItem from './TripItem';
 
 export {
-  Search,
-  Country,
   Continent,
-  Layer,
-  Tab,
-  Profile,
-  Select,
+  Country,
   Currency,
   Expense,
-  Report,
   Header,
+  Layer,
+  Profile,
+  Report,
+  Search,
+  Select,
+  Tab,
   TripItem
 };
