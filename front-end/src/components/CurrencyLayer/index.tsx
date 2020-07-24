cfeca7929ef6404a89977e933cc5139edbb7beac
전솔 <jeonsol12@naver.com>
2020-07-24 16:28:15 +0900

    [front-end] currency Layer 마크업

--- front-end/src/components/CurrencyLayer/index.tsx
+++ front-end/src/components/CurrencyLayer/index.tsx
@@ -15,24 +15,38 @@ const CurrencyLayer = () => {
           <strong className={cx('title')}>KWW</strong>
         </div>
         <div className={cx('currency_info')}>
-          <strong className={cx('tilte')}>예산 정보</strong>
-          <dl className={cx('detail_area')}>
-            <dt>화폐</dt>
-            <dd>KRW</dd>
-            <dt>환율</dt>
-            <dd>KRW 1 = KRW 1</dd>
-          </dl>
+          <strong className={cx('title')}>예산 정보</strong>
+          <ul className={cx('detail_area')}>
+            <li className={cx('detail_list')}>
+              <span className={cx('detail_title')}>화폐</span>
+              <span className={cx('detail_text')}>KRW</span>
+            </li>
+            <li className={cx('detail_list')}>
+              <span className={cx('detail_title')}>환율</span>
+              <button type="button" className={cx('detail_text')}>KRW 1 = KRW 1</button>
+            </li>
+          </ul>
         </div>
         <div className={cx('currency_amount')}>
+          <div className={cx('amount_title_area')}>
+            <strong className={cx('title')}>예산 금액</strong>
+            <span className={cx('amount')}>#258</span>
+          </div>
           <button className={cx('add_currency')}>예산 금액 추가하기</button>
-          <dl className={cx('curreny_list')}>
-            <dt>[2020.4.18.]예산</dt>
-            <dd>+ 8,888, 888.00</dd>
-            <dt>[2020.4.18.]예산</dt>
-            <dd>+ 8,888, 888.00</dd>
-          </dl>
+          <div className={cx('curreny_list')}>
+            <button type="button" className={cx('list_item')}>
+              <span className={cx('list_title')}>[2020.4.18.] 예산</span>
+              <span className={cx('list_text')}>+ 8,888, 888.00</span>
+            </button>
+            <button type="button" className={cx('list_item')}>
+              <span className={cx('list_title')}>[2020.4.18.] 예산</span>
+              <span className={cx('list_text')}>+ 8,888, 888.00</span>
+            </button>
+          </div>
         </div>
-        <button className={cx('btn_delete')}>이 예산 삭제하기</button>
+        <div className={cx('btn_delete_area')}>
+          <button className={cx('btn_delete')}>이 예산 삭제하기</button>
+        </div>
       </div>
     </div>
   )
