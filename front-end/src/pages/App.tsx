5e3d7c47acc2555d091aa691a969dd9aea2a1ab8
전솔 <jeonsol@nhn.com>
2020-10-08 15:50:12 +0900

    수정

--- front-end/src/pages/App.tsx
+++ front-end/src/pages/App.tsx
@@ -34,7 +34,7 @@ const Main = () => {
         <meta name="twitter:card" content="summary_large_image" />
         <title>네이버 오토 브랜드 위크</title>
       </Helmet>
-      {isLogin ? (
+      {/* {isLogin ? (
         <>
         <HashRouter>
           <Suspense fallback={Loading()}>
@@ -54,7 +54,7 @@ const Main = () => {
         </>
       ) : (
         <Login />
-      )}
+      )} */}
     </>
   );
 };
